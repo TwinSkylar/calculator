7a5518371762e83b618d3299f5eaa0f8e1352457
John Luu <122046486+TwinSkylar@users.noreply.github.com>
2023-01-26 18:11:54 -0500

    The basic shell of a calculator.

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function add(a, b) { return a + b; }
+function subtract(a, b) { return a - b; }
+function multiply(a, b) { return a * b; }
+function divide(a, b) { return a / b; }
+
+function operate(operator, a, b) {
+    result = 0;
+    switch (operator) {
+        case add:
+            result = add(a, b);
+            break;
+        case subtract:
+            result = subtract(a, b);
+            break;
+        case multiply:
+            result = multiply(a, b);
+            break;
+        case divide:
+            result = divide(a, b);
+            break;
+    }
+    return result;
+}
